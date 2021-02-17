82d5879077b7599dbe1fc683b2ba93006a2986ae
Vic VCT <vvincent@it-students.fr>
2021-02-17 10:07:03 +0100

    change CSS / HTML

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//rien
+let restroom;
+
+
+fetch('https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=10&offset=0&query=lyon%20fr')
+.then(function(response) {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
