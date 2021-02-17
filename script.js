0c05647f469baddfe302c87d3bcac0a79577139d
Lionnel Traverse <tlionnel@gmail.com>
2021-02-17 17:00:16 +0100

    add map js/html/css

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let cities = [
     {name: 'Lille', value: 'lille%20FR'},
     {name: 'Montpellier', value: 'montpellier%20FR'},
     {name: 'Bruxelles', value: 'brussels%20BE'},
-    {name: 'Barcelone', value: 'london%20GB'},
+    {name: 'Londres', value: 'london%20GB'},
     {name: 'Berlin', value: 'berlin%20DE'},
     {name: 'Barcelone', value: 'barcelona%20ES'}
 ]
@@ -20,7 +20,7 @@ cities.forEach(city=>{
     option.value = city.value;
     list.add(option);
     console.log(city);
-
+  
 })
 // let cityChoice = document.querySelector('class');
 
@@ -29,7 +29,7 @@ document.querySelector('#cities').addEventListener('change', (el) => {
     url = el.target.value;
     log();
 });
-
+let local;
 function log(){
     console.log(url);
     //mettre le fetch ici
@@ -39,11 +39,10 @@ function log(){
     })
     .then((data) => {
         let displayList = document.querySelector('.nav');
-        let location = data;
+        local = data;
         let ul = document.createElement('ul');
-        location.forEach(info => {
+        local.forEach(info => {
             displayList.innerHTML = '';
-
             let li = document.createElement('li');
             console.log(info);
             li.innerHTML = info.name;
@@ -52,56 +51,75 @@ function log(){
         })
         displayList.appendChild(ul);
     })
+    return local;
 }
-// .then((response) => {
-// fetch('https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=100&offset=0&query='+value)
 
 
 document.querySelector('.nav').addEventListener('click', (el) => {
-    el = el.target;
-    if (el.dataset.id) {
-        fetch (variable)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            document.querySelector("#name").innerHTML = data.name;
-            document.querySelector("#city").innerHTML = data.city;
-            document.querySelector("#street").innerHTML = data.street;
-            document.querySelector("#country").innerHTML = data.country;
-            document.querySelector("#accessible").innerHTML = data.accessible;
-            document.querySelector("#unisex").innerHTML = data.unisex;
-            info.classList.remove('none');
-        });
+    el = el.target.dataset.id;
+    console.log(el);
+    let result;
+    if (el) {
+        result = local.find (id => id.id == el);
+        console.log(result);
+        document.querySelector("#name").innerHTML = result.name;
+        document.querySelector("#city").innerHTML = result.city;
+        document.querySelector("#street").innerHTML = result.street;
+        document.querySelector("#country").innerHTML = result.country;
+        document.querySelector("#accessible").innerHTML = result.accessible;
+        document.querySelector("#unisex").innerHTML = result.unisex;
+        //info.classList.remove('none');
     }
+    var container = L.DomUtil.get('mapid');
+    if(container != null){
+    container._leaflet_id = null;
+    }
+
+    let lon = result.longitude;
+    let lat = result.latitude;
+
+    let mymap = L.map('mapid').setView([lat, lon], 13);
+    let marker = L.marker([lat, lon]).addTo(mymap);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+        })
+        .addTo(mymap);
+    //console.log(local);
 })
 
 
 
 
-// // let city;
-// // fetch(' https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=100&offset=0&query=paris%20FR') // requète sur l'API
-// // .then((response)=>{ //le => remplace cette syntax: .then(function(response)...)
-// //     return response.json(); // renvoie le résultat sous forme de .json
-// //     // console.log(response);
-// // })
 
-// // .then((data)=>{ //ce que tu me retourne appelle le data
-// //     city = data; 
-// //     city.forEach(i=>{
-// //         console.log(i.country);
+// let city;
+// fetch(' https://www.refugerestrooms.org/api/v1/restrooms/search?page=1&per_page=100&offset=0&query=paris%20FR') // requète sur l'API
+// .then((response)=>{ //le => remplace cette syntax: .then(function(response)...)
+//     return response.json(); // renvoie le résultat sous forme de .json
+//     // console.log(response);
+// })
 
-// //     })
+// .then((data)=>{ //ce que tu me retourne appelle le data
+//     city = data; 
+//     city.forEach(i=>{
+//         console.log(i.country);
+
+//     })
     
-// // })
+// })
 
-// // document.querySelector('.character').addEventListener("click", (el) =>{
-// //     el=el.target.id;
-// //     let characterData = characters[el];
-// //     console.log(el);
-// //     // console.log(characterData);
-// //     characterData.forEach(info =>{
-// //         console.log(info);
+// document.querySelector('.character').addEventListener("click", (el) =>{
+//     el=el.target.id;
+//     let characterData = characters[el];
+//     console.log(el);
+//     // console.log(characterData);
+//     characterData.forEach(info =>{
+//         console.log(info);
 
-// //     })
-// // })
+//     })
+// })
